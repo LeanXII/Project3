5a64cab7d78096d6e91543a303eb9e8ed793b272
santiagojoejr <santiagojoejr@gmail.com>
2024-11-22 14:01:56 -0800

    added faker file to seed file

--- api/seeds/user_data.js
+++ api/seeds/user_data.js
@@ -1,13 +1,23 @@
+const {faker} = require('@faker-js/faker')
+
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> } 
  */
+
+
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
-  await knex('users_table').del()
-  await knex('users_table').insert([
-    {username: 'joe', password: '1234!@#$'},
-    {username: 'josh', password: '4321$#@!'},
-    {username: 'brandon_addy', password: '1223$%^&'}
-  ]);
+  await knex('users').del()
+
+  const fakeData = (entries) => {
+    var results = []
+    
+    for (let i = 0; i < entries; i++) {
+      let user = {username: faker.internet.username(), password: faker.internet.password()}
+      results.push(user)
+    }
+    return results;
+  }
+  await knex('users').insert(fakeData(100));
 };

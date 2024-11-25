fa6796ebfeb8cb99f6d310d6f02eec175d739bf5
sixftallstar314 <brandon.d.garrett11@gmail.com>
2024-11-25 16:32:10 -0500

    updating branch with main and framework nearby cities

--- ui/src/components/SearchLocation.jsx
+++ ui/src/components/SearchLocation.jsx
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 function SearchLocation() {
   const [searchText, setSearchText] = React.useState('');
   const [searchResults, setSearchResults] = React.useState(null);
   const [previousSearches, setPreviousSearches] = React.useState([]);
+  const [nearbyCities, setNearbyCities] = React.useState([]);
+
+  // useEffect(() => {
+  //   fetch('https://api.ipify.org?format=json')
+  //     .then(response => response.json())
+  //     .then(data => {
+  //       const ip = data.ip;
+  //       return fetch(`https://ipapi.co/${ip}/json/`);  
+  //     })
+  //     .then(response => response.json())
+  //     .then(data => {
+  //       const { latitude, longitude } = data;
+  //       fetchNearbyCities(latitude, longitude);
+  //     })
+  //     .catch(error => {
+  //       console.error('Error fetching user location:', error);
+  //     });
+  // }, []);
+
+  // const fetchNearbyCities = (latitude, longitude) => {
+  //   const nearbyCitiesUrl = `https://wft-geo-db.p.rapidapi.com/v1/geo/locations/${latitude}+${longitude}/nearbyCities?radius=100`;
+
+  //   fetch(nearbyCitiesUrl, {
+  //     method: 'GET',
+  //     headers: {
+  //       'x-rapidapi-key': 'd227ab3bdfmsh384e9f6c4cdd58ap165d02jsn5402cbec7415',
+  //       'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com',
+  //     },
+  //   })
+  //   .then(response => response.json())
+  //   .then(data => {
+  //     setNearbyCities(data.data);
+  //   })
+  //   .catch(error => {
+  //     console.error('Oops, something went wrong fetching nearby cities:', error);
+  //   });
+  // };
 
   function handleSearchButton(event) {
     event.preventDefault();
@@ -19,17 +56,17 @@ function SearchLocation() {
         'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com',
       },
     })
-      .then(response => response.json())
-      .then(data => {
-        const filterResults = data.data.filter(city => city.population > 0);
-        setSearchResults(filterResults);
-        if (!previousSearches.includes(searchTextValue)) {
-          setPreviousSearches([...previousSearches, searchTextValue]);
-        }
-      })
-      .catch(error => {
-        console.log('Oops, something went wrong:', error);
-      });
+    .then(response => response.json())
+    .then(data => {
+      const filterResults = data.data.filter(city => city.population > 0);
+      setSearchResults(filterResults);
+      if (!previousSearches.includes(searchTextValue)) {
+        setPreviousSearches([...previousSearches, searchTextValue]);
+      }
+    })
+    .catch(error => {
+      console.log('Oops, something went wrong:', error);
+    });
   }
 
   let previousSearchList = null;
@@ -43,6 +80,22 @@ function SearchLocation() {
               <button onClick={() => handlePreviousSearch(search)}>
                 {search}
               </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+
+  let nearbyCitiesList = null;
+  if (nearbyCities.length > 0) {
+    nearbyCitiesList = (
+      <div>
+        <h2>Nearby Cities:</h2>
+        <ul>
+          {nearbyCities.map((city, index) => (
+            <li key={index}>
+              City: {city.city}, Country: {city.countryCode}, Region: {city.region}, Population: {city.population}
             </li>
           ))}
         </ul>
@@ -64,6 +117,7 @@ function SearchLocation() {
       
       <div>
         {previousSearchList}
+        {nearbyCitiesList}
       </div>
 
       <div>

8bd7e57c539c5a69f1720427320afcf1bd578dda
VoidZephyr <jking21412@gmail.com>
2024-11-26 07:41:36 -0800

    dockerrestart

--- ui/src/components/SuggestedLocations.jsx
+++ ui/src/components/SuggestedLocations.jsx
@@ -1,46 +1,46 @@
 import { useEffect, useState } from "react";
 import TouristAttractions from "./TouristAttractions";
-import {nearbySuggestions} from '../../fakeUIData.js'
+//import {nearbySuggestions} from '../../fakeUIData.js'
 import '../stylesheets/SuggestedLocations.css'
 
 const apiKey = import.meta.env.VITE_API_KEY;
 
 function SuggestedLocations() {
-  // const [clientLocationObj, setClientLocationObj] = useState({});
-  // const [nearbySuggestions, setNearbySuggestions] = useState({});
+  const [clientLocationObj, setClientLocationObj] = useState({});
+  const [nearbySuggestions, setNearbySuggestions] = useState({});
 
-  //Request to backend -> backend to ipGrabber and back -> request to geoDbapi
-  //Likely a better way to deal with slow server spin up than a setTimeout
+  // Request to backend -> backend to ipGrabber and back -> request to geoDbapi
+  // Likely a better way to deal with slow server spin up than a setTimeout
 
-  // useEffect(() => {
-  //   const ipFetch = async () => {
-  //     try {
-  //       let response = await fetch("http://localhost:3000/home");
-  //       if (response.ok) {
-  //         response = await response.json();
-  //         setClientLocationObj(response);
-  //         fetch(
-  //           `https://wft-geo-db.p.rapidapi.com/v1/geo/locations/${response?.lat}${response?.lon}/nearbyPlaces?limit=3&offset=0&types=CITY&radius=100&sort=-population,name`,
-  //           {
-  //             headers: {
-  //               "x-rapidapi-key": apiKey,
-  //               "x-rapidapi-host": "wft-geo-db.p.rapidapi.com",
-  //             },
-  //           }
-  //         )
-  //           .then((res) => res.json())
-  //           .then((data) => {
-  //             console.log(data);
-  //             setNearbySuggestions(data);
-  //           });
-  //       }
-  //     } catch (err) {
-  //       console.log(err);
-  //     }
-  //   };
+  useEffect(() => {
+    const ipFetch = async () => {
+      try {
+        let response = await fetch("http://localhost:3000/home");
+        if (response.ok) {
+          response = await response.json();
+          setClientLocationObj(response);
+          fetch(
+            `https://wft-geo-db.p.rapidapi.com/v1/geo/locations/${response?.lat}${response?.lon}/nearbyPlaces?limit=3&offset=0&types=CITY&radius=100&sort=-population,name`,
+            {
+              headers: {
+                "x-rapidapi-key": apiKey,
+                "x-rapidapi-host": "wft-geo-db.p.rapidapi.com",
+              },
+            }
+          )
+            .then((res) => res.json())
+            .then((data) => {
+              console.log(data);
+              setNearbySuggestions(data);
+            });
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    };
 
-  //   setTimeout(ipFetch, 3000);
-  // }, []);
+    setTimeout(ipFetch, 3000);
+  }, []);
 
   return (
     nearbySuggestions?.data && (
--- ui/src/components/TouristAttractions.jsx
+++ ui/src/components/TouristAttractions.jsx
@@ -1,6 +1,6 @@
 import { WBK } from "wikibase-sdk";
 import { useState, useEffect } from "react";
-import { attractions } from "../../fakeUIData.js";
+//import { attractions } from "../../fakeUIData.js";
 import "../stylesheets/TouristAttractions.css";
 
 const wbk = WBK({
@@ -9,30 +9,30 @@ const wbk = WBK({
 });
 
 const TouristAttractions = ({ wikiDataCityId }) => {
-  // const [attractions, setAttractions] = useState({});
+  const [attractions, setAttractions] = useState({});
 
-  // useEffect(() => {
-  //   const sparql = `SELECT DISTINCT ?attraction ?attractionLabel ?gps
-  //   WHERE {
-  //     ?attraction (wdt:P31/wdt:P279*) wd:Q570116; wdt:P625
-  //     ?gps; rdfs:label
-  //     ?attractionLabel. ?attraction wdt:P131 wd:${wikiDataCityId}
-  //     FILTER(LANG(?attractionLabel) = 'en')}`;
-  //   console.log(wikiDataCityId)
-  //   const url = wbk.sparqlQuery(sparql);
-  //   const headers = { "Api-User-Agent": "jctuttle42@gmail.com" };
+  useEffect(() => {
+    const sparql = `SELECT DISTINCT ?attraction ?attractionLabel ?gps
+    WHERE {
+      ?attraction (wdt:P31/wdt:P279*) wd:Q570116; wdt:P625
+      ?gps; rdfs:label
+      ?attractionLabel. ?attraction wdt:P131 wd:${wikiDataCityId}
+      FILTER(LANG(?attractionLabel) = 'en')}`;
+    console.log(wikiDataCityId)
+    const url = wbk.sparqlQuery(sparql);
+    const headers = { "Api-User-Agent": "jctuttle42@gmail.com" };
 
-  //   const requestWikiData = async () => {
-  //     let response = await fetch(url, {
-  //       headers: headers,
-  //     });
-  //     response = await response.json();
+    const requestWikiData = async () => {
+      let response = await fetch(url, {
+        headers: headers,
+      });
+      response = await response.json();
 
-  //     console.log("This is the wikidata response:", response);
-  //     setAttractions(response);
-  //   };
-  //   requestWikiData();
-  // }, []);
+      console.log("This is the wikidata response:", response);
+      setAttractions(response);
+    };
+    requestWikiData();
+  }, []);
 
   return (
     attractions?.results?.bindings && (
